d775fdd3075341d336d3f5165c6805d142ad9b6a
anpenalo <anpenalo@bancolombia.com.co>
2024-03-01 22:33:09 -0500

    begin frontend

---
+++ Order/Orders.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Orders.Frontend.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpActionResponseMessage)
+        {
+            Response = response;
+            Error = error;
+            HttpResponseMessage = httpActionResponseMessage;
+        }
+
+        public T? Response { get; }
+        public bool Error { get; }
+        public HttpResponseMessage HttpResponseMessage { get; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado.";
+            }
+            if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que estar logueado para ejecutar esta operación.";
+            }
+            if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para hacer esta operación";
+            }
+            return "Error desconocido";
+        }
+    }
+}
